6a66d0f068b46fb579d672ed04bfb8fe6bb7a891
Pedro Felipe Teixeira <pedrofelipest@gmail.com>
2021-03-25 22:02:10 -0300

    Inclusão Aula 4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.options.pipeline_options import PipelineOptions
@@ -46,6 +47,18 @@ def chave_uf(elemento):
     chave = elemento['uf']
     return (chave, elemento)
 
+def casos_dengue(elemento):
+    """
+    Recebe uma tupla ('RS', [{}, {}])
+    Retornar uma tupla ('RS-2014-12', 8.0)
+    """
+    uf, registros = elemento
+    for registro in registros:
+        if bool(re.search(r'\d', registro['casos'])):
+            yield (f"{uf}-{registro['ano_mes']}", float(registro['casos']))
+        else:
+            yield (f"{uf}-{registro['ano_mes']}", 0.0)
+
 dengue = (
     pipeline
     | "Leitura do dataset de dengue" >>
@@ -55,6 +68,16 @@ dengue = (
     | "Criar campo ano_mes" >> beam.Map(trata_datas)
     | "Criar chave pelo estado" >> beam.Map(chave_uf)
     | "Agrupar pelo estado" >> beam.GroupByKey()
+    | "Descompactar casos de dengue" >> beam.FlatMap(casos_dengue)
+    | "Soma dos casos pela chave" >> beam.CombinePerKey(sum)
+    # | "Mostrar resultados" >> beam.Map(print)
+)
+
+chuvas = (
+    pipeline
+    | "Leitura do dataset de chuvas" >>
+        ReadFromText('sample_chuvas.csv', skip_header_lines=1)
+    | "De texto para lista (chuvas)" >> beam.Map(texto_para_lista, delimitador=',')
     | "Mostrar resultados" >> beam.Map(print)
 )
 
